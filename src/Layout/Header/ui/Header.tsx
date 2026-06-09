4842ed69b79be3bdac65ff51c98a5a42707feaa5
ilya <shcherbakov.illya@list>
2026-06-09 19:27:04 +0400

    first commit

---
+++ src/Layout/Header/ui/Header.tsx
@@ -0,0 +1,18 @@
+import TopBar from "./TopBar";
+import './header.scss'
+
+const Header = () => {
+    return (
+        <header className="header">
+            <div className="header__top">
+                <TopBar />
+            </div>
+            
+            
+        </header>
+    )
+        
+    
+}
+
+export default Header;
